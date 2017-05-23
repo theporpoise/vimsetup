cdc59b55cba0599edb24b199a2376ad9563a574c
matthew <matt@everettgould.com>
2017-05-23 10:06:11 -0700

    script to setup zsh and ohmyzsh.  may need tweaking.  also noticed issues with vim ikn a lot of places with my vimrc.  updating script there to simply copy it into the main director and rename this folder.

---
+++ shellsetup.sh
@@ -0,0 +1,6 @@
+sudo apt-get vim
+sudo apt-get install zsh
+sudo apt-get install git-core
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s `which zsh`
+sudo shutdown -r 0
